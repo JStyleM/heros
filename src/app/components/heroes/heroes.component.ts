1aeb0423b93c37b1f32aa118d8ce4e52d21eb4e7
Jhonatan Moncada <jhonatanmoncada@gmail.com>
2018-11-06 13:29:54 -0500

    add btn ver mas functionality

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,13 +10,18 @@ import { HeroesService, Heroe } from '../../services/heroes.service';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor( private _heroesService: HeroesService) { }
+  constructor( private _heroesService: HeroesService,
+               private router: Router) { }
 
   heroes: Heroe[] = [];
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
+  }
+
+  verHeroe( idx: number ) {
+    this.router.navigate(['/heroe', idx]);
   }
 
 }
